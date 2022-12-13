2131db1ff9aa57114d68032c8a61bed648706b94
Mohamed Ahmed <mohamaziz606@gmail.com>
2022-12-13 09:21:25 +0200

    Cholesky decomposition

--- src/com/codebind/EquationsSolver.java
+++ src/com/codebind/EquationsSolver.java
@@ -10,11 +10,25 @@ public class EquationsSolver {
         equations[1] = new Equation(new double[]{4, 1, 4}, 9);
         equations[2] = new Equation(new double[]{3, 4, 6}, 0);
 
-        //        ----------------
-        //        testing LU
+        Equation[] equations2 = new Equation[3];
+        equations2[0] = new Equation(new double[]{6, 15, 55}, -4);
+        equations2[1] = new Equation(new double[]{15, 55, 225}, 9);
+        equations2[2] = new Equation(new double[]{55, 225, 979}, 0);
+
+        // testing cholesky decomposition
+        CholeskyDecompositioin test3 = new CholeskyDecompositioin(equations2, 0.0001);
+        if(test3.getSolution() == null)
+            System.out.println("you must provide symmetric matrix to apply cholesky decomposition");
+        else {
+            double[] sol3 = test3.getSolution();
+            System.out.println(Arrays.toString(sol3));
+        }
+
+        // testing LU
         LinearSolver test2 = new LUDecomposition(equations, 0.0001);
         double[] sol2 = test2.getSolution();
         System.out.println(Arrays.toString(sol2));
+
         //LinearSolver test = new Jacobi(equations, new double[]{1, 1, 1}, 50, 0.1);
         LinearSolver test = new Gauss_Jordan(equations);
         double[] sol = test.getSolution();
